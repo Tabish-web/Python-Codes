34d5c628dec038d6417f3df3dfa6f83eedf1d1c6
Tabish-web <114544578+Tabish-web@users.noreply.github.com>
2025-02-06 13:50:55 +0530

    Update 02_datatypes.py

--- 02_datatypes.py
+++ 02_datatypes.py
@@ -1,8 +1,8 @@
 a = 1 # a is an integer
 
-b = 5.22 # b is a floating point number
+b = 3.20 # b is a floating point number
 
-c = "Harry" # c is a string
+c = "Shaha" # c is a string
 
 d = False # d is a boolean variable
 
